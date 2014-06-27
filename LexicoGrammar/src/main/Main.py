06bf3905d77c8944bf99fd2ec6edd1e4f93be8a3
brudelle <brudelle1@gmail.com>
2014-06-26 20:03:56 -0400

    test juste un commentaire qui sert a rien

--- LexicoGrammar/src/main/Main.py
+++ LexicoGrammar/src/main/Main.py
@@ -1,7 +1,7 @@
 '''
 Created on 23 juin 2014
 
-@author: Charly
+@author: Charly 
 '''
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
@@ -10,7 +10,7 @@ from nltk.chunk import ne_chunk
 from nltk.tag.util import str2tuple
 
 exemples = ["And now, you don't do something completely different.",'San Francisco is foggy']
-
+#asgjklsfsdjkl
 
 def printExemple(sentence):
     
@@ -42,7 +42,7 @@ def main():
 def prepareTagsData():
     
     'download tagging'
-    downloader.download()
+    #downloader.download()
     #===========================================================================
     # print('download all tags')
     # downloader.download('all',"D:\\nltk_data",True)
@@ -55,6 +55,6 @@ if __name__ == '__main__':
     prepareTagsData()
     
     #===========================================================================
-    # main()
+     main()
     #===========================================================================
 
